5bfbf1a20b49c2376e03655a4cd5adf5cc74f9e1
ollie314 <mehdi.lefebvre@gmail.com>
2012-11-26 11:49:14 +0100

    New Test

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,26 +1,132 @@
 /**
  *
  */
-function onDeviceReady() {
-	Simnet.Logger.activate();
-	$.support.cors = true;
-	$.mobile.showPageLoadingMsg();
-	$( '#notificationContainer' ).toast();
+// APP
+var app = {
+    bodyLoad:function(){
+        document.addEventListener("deviceready", app.deviceReady, false);
+    },
+    deviceReady:function(){
+        app.init();
+    },
+    init:function(){
+        // First lets check to see if we have a user or not
+        if(!localStorage.getItem(facebook_token)){
+            $("#fbAction").attr('data-theme', 'b');
+            $("#status").hide();
 
-	SimFacebook.init();
+            $("#fbAction").click(function(){
+                Facebook.init();
+            });
+        }
+        else {
+            Simnet.Logger.debug("showing loged in");
+            $("#loginArea").hide();
+            $("#status").show();
+            $("#statusBTN").click(function(){
+                createPost();
+            });
+        }
+    },
+    done:function(){
 
-	$( "#loginButton" ).click( function( mouseEvent ) {
-		// test simapp & social functions
-		SimFacebook.postMessage( "This is a simple test" );
-                return false;
-	} );
+    },
+    createPost:function(){
 
-        $( "#fbAction" ).click( function( mouseEvent ) {
-            SimFacebook.postMessage( "This is a simple test from the application ..." );
-            return false;
-        } );
+
+        // Define our message!
+        var msg = $("#statusTXT").val();
+
+        // Define the part of the Graph you want to use.
+        var _fbType = 'feed';
+
+        // This example will post to a users wall with an image, link, description, text, caption and name.
+        // You can change
+        var params = {};
+        params['message'] = msg;
+        params['name'] = 'A Facebook App for Phonegap';
+        params['description'] = "I just made a Facebook app with Phonegap using this sweet tutorial from Drew Dahlman";
+        params['_link'] = "http://www.drewdahlman.com";
+        params['picture'] = "http://compixels.com/wp-content/uploads/2011/04/Facebook-Logo.jpg";
+        params['caption'] = 'Hello World';
+
+        // When you're ready send you request off to be processed!
+        Facebook.post(_fbType,params);
+    }
+},
+createPost = function(){
+
+
+    // Define our message!
+    var msg = "This is a test !!!";
+
+    // Define the part of the Graph you want to use.
+    var _fbType = 'feed';
+
+    // This example will post to a users wall with an image, link, description, text, caption and name.
+    // You can change
+    var params = {};
+    params['message'] = msg;
+    params['name'] = 'Another day of labour ... ;)';
+    params['description'] = "Retrying to do some things";
+    params['_link'] = "http://www.simnetsa.ch";
+    params['picture'] = "http://www.simnetsa.ch/themes/simnetsa//images/logo.png";
+    params['caption'] = 'Is it Working ???';
+
+    // When you're ready send you request off to be processed!
+    Facebook.post(_fbType,params);
+},
+onDeviceReady = function() {
+    Simnet.Logger.activate();
+    $.support.cors = true;
+    $.mobile.showPageLoadingMsg();
+    $( '#notificationContainer' ).toast();
+
+    //SimFacebook.init();
+
+    $( "#loginButton" ).click( function( mouseEvent ) {
+        // test simapp & social functions
+        SimFacebook.postMessage( "This is a simple test" );
+        return false;
+    } );
+
+    $( "#fbAction" ).click( function( mouseEvent ) {
+        SimFacebook.postMessage( "This is a simple test from the application ..." );
+        return false;
+    } );
 };
 
 ( function( $ ) {
-	document.addEventListener("deviceready", onDeviceReady, false);
+    document.addEventListener("deviceready", onDeviceReady, false);
 } )( jQuery );
+
+if( false ) {
+    window.fbAsyncInit = function() {
+        // param to initialize the fb api
+        FB.init( {
+            appId      : SimFacebook.API_ID,
+            channelUrl : SimFacebook.CHAN_URL,
+            status     : true,
+            cookie     : true,
+            xfbml      : false
+        } );
+
+        SimFacebook.fetchLoginStatus();
+        $( document ).trigger( SimApp.events.FB_APPLICATION_INITIALIZING );
+
+    }; /* fbAsyncInit */
+
+    // load the facebook api and run it.
+    ( function( doc, dbg ){
+        var js = doc.createElement( 'script' ),
+        id = 'facebook-jssdk',
+        ref = doc.getElementsByTagName( 'script' )[0];
+        if( doc.getElementById( id ) ) {
+            return;
+        }
+        js.id = id;
+        js.async = true;
+        js.src = "//connect.facebook.net/en_US/all" + ( dbg ? "/debug" : "" ) + ".js";
+        ref.parentNode.insertBefore( js, ref );
+    }( document, true ) );
+}
---
+++ assets/js/social/facebook.js
@@ -0,0 +1,104 @@
+/*
+ *
+ */
+var my_client_id = "490104927689454", // YOUR APP ID
+    my_secret = "53fb1242e82413962563b378bd6260f9", // YOUR APP SECRET
+    my_redirect_uri = "http://www.facebook.com/connect/login_success.html", // LEAVE THIS
+    my_type ="user_agent",
+    my_display = "touch"; // LEAVE THIS
+
+var facebook_token = "fbToken"; // OUR TOKEN KEEPER
+var client_browser;
+
+// FACEBOOK
+var Facebook = {
+    init:function(){
+        // Begin Authorization
+        var authorize_url = "https://graph.facebook.com/oauth/authorize?";
+        authorize_url += "client_id=" + my_client_id;
+        authorize_url += "&redirect_uri=" + my_redirect_uri;
+        authorize_url += "&display=" + my_display;
+        authorize_url += "&scope=publish_stream,offline_access"
+
+        // Open Child browser and ask for permissions
+        client_browser = ChildBrowser.install();
+        client_browser.onLocationChange = function(loc){
+            Facebook.facebookLocChanged(loc);
+        };
+        if (client_browser != null) {
+            window.plugins.childBrowser.showWebPage(authorize_url);
+        }
+    },
+    facebookLocChanged:function(loc){
+
+        // When the childBrowser window changes locations we check to see if that page is our success page.
+        if (loc.indexOf("http://www.facebook.com/connect/login_success.html") > -1) {
+            var fbCode = loc.match(/code=(.*)$/)[1]
+            $.ajax({
+                url:'https://graph.facebook.com/oauth/access_token?client_id='+my_client_id+'&client_secret='+my_secret+'&code='+fbCode+'&redirect_uri=http://www.facebook.com/connect/login_success.html',
+                data: {},
+                dataType: 'text',
+                type: 'POST',
+                success: function(data, status){
+
+                    // We store our token in a localStorage Item called facebook_token
+                    localStorage.setItem(facebook_token, data.split("=")[1]);
+
+                    window.plugins.childBrowser.close();
+
+                    app.init();
+                },
+                error: function(error) {
+                    window.plugins.childBrowser.close();
+                }
+            });
+        }
+    },
+    share:function(url){
+
+        // Create our request and open the connection
+        var req = new XMLHttpRequest();
+        req.open("POST", url, true);
+
+
+        req.send(null);
+        return req;
+    },
+    post:function(_fbType,params){
+
+        // Our Base URL which is composed of our request type and our localStorage facebook_token
+        var url = 'https://graph.facebook.com/me/'+_fbType+'?access_token='+localStorage.getItem(facebook_token);
+
+        // Build our URL
+        for(var key in params){
+            if(key == "message"){
+
+                // We will want to escape any special characters here vs encodeURI
+                url = url+"&"+key+"="+escape(params[key]);
+            }
+            else {
+                url = url+"&"+key+"="+encodeURIComponent(params[key]);
+            }
+        }
+
+        var req = Facebook.share(url);
+
+        // Our success callback
+        req.onload = Facebook.success();
+    },
+    success:function(){
+        $("#statusTXT").show();
+        $("#statusBTN").show();
+
+        // hide our info
+        $("#info").hide();
+
+        // reset our field
+        $("#statusTXT").val('');
+
+        console.log("DONE!");
+
+    }
+};
+
+
