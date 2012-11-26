cd05cbd00fe0538839e6ff8e08a670dc6c0c5e7d
ollie314 <mehdi.lefebvre@gmail.com>
2012-11-26 12:16:55 +0100

    add facebook utility methods

--- assets/js/social/facebook.js
+++ assets/js/social/facebook.js
@@ -12,22 +12,24 @@ var client_browser;
 
 // FACEBOOK
 var Facebook = {
-    init:function(){
-        // Begin Authorization
+    get_authorize_url : function() {
         var authorize_url = "https://graph.facebook.com/oauth/authorize?";
         authorize_url += "client_id=" + my_client_id;
         authorize_url += "&redirect_uri=" + my_redirect_uri;
         authorize_url += "&display=" + my_display;
         authorize_url += "&scope=publish_stream,offline_access"
 
+        return authorize_url;
+    },
+    init:function(){
+        // Begin Authorization
+        var authorize_url = Facebook.get_authorize_url();
         // Open Child browser and ask for permissions
         //client_browser = ChildBrowser.install();
-        client_browser.onLocationChange = function(loc){
+        window.plugins.childBrowser.onLocationChange = function(loc){
             Facebook.facebookLocChanged(loc);
         };
-        if (client_browser != null) {
-            window.plugins.childBrowser.showWebPage(authorize_url);
-        }
+        window.plugins.childBrowser.showWebPage(authorize_url);
     },
     facebookLocChanged:function(loc){
 
